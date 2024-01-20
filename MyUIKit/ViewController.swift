ca15cf786eddd7ff8bef1f07862b01871490e969
Dyani <dyani70@gmail.com>
2024-01-20 23:23:13 +0900

    ViewController

--- MyUIKit/ViewController.swift
+++ MyUIKit/ViewController.swift
@@ -9,12 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myButton: UIButton!
+    @IBOutlet weak var myLabel: UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .blue
+        view.backgroundColor = .green  // view를 조정
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func didMyButtonTapped(_ sender: Any) {
+        print("Hello leeo!!!")
+    }
+    
 }
 
