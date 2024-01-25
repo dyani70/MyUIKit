fcd07e4ac2e7fc00d9b3bf953a023442b699242b
Dyani <dyani70@gmail.com>
2024-01-25 23:12:38 +0900

    UITableView Delegate

--- MyUIKit/MyTableViewController.swift
+++ MyUIKit/MyTableViewController.swift
@@ -21,7 +21,7 @@ class MyTableViewController: UIViewController {
         
     }
     
-    func configureTableView() {
+    func configureTableView() { //함수 정의하고 위에서 호출
         myTableView.backgroundColor = .green
         myTableView.delegate = self
         myTableView.dataSource = self
@@ -46,15 +46,27 @@ extension MyTableViewController: UITableViewDataSource {
         return UITableViewCell()
     }
     
-    
     func numberOfSections(in tableView: UITableView) -> Int {
         return 3
+    } //3번 리턴
+    //함수를 정의하고 호출하지 않았는데도 실행됌
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        if indexPath.section == 0 {
+            return 200
+        } else {
+            return 60.0
+        }
     }
+    
     
     func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
         return true
     }
     
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
     
     
 }
